7c0a8182e2cdc1e94af53b5889ccfff6d594faa9
Nicolas Betim <nicolasbetim08@gmail.com>
2022-07-12 14:55:46 -0300

    Exercicio de Lista

---
+++ ex81.py
@@ -0,0 +1,14 @@
+valores = []
+while True:
+    valores.append(int(input('Digite um valor: ')))
+    resp = str(input('Quer continuar? [S/N]'))
+    if resp in 'Nn':
+        break
+print('-='*30)
+print(f'Você digitou {len(valores)} elementos.')
+valores.sort(reverse=True)
+print(f'Os valores em ordem decrescente são {valores}')
+if 5 in valores:
+    print('O número 5 esta entre a lista!')
+else:
+    print('O número 5 não se encontra na lista!')
