9cb09c08394d50ff4557f9370b2eeedb0944e888
Nicolas Betim <nicolasbetim08@gmail.com>
2022-08-04 10:08:25 -0300

    Exercicio de Função

---
+++ ex96.py
@@ -0,0 +1,10 @@
+def area(area):
+    area = larg * comp
+    print(f'A área de um terreno {larg}x{comp} é de {area:.1f}²')
+
+
+print('Controle de Terrenos')
+print('-' * 40)
+larg = float(input('LARGURA (m): '))
+comp = float(input('COMPRIMENTO (m): '))
+area(area)
